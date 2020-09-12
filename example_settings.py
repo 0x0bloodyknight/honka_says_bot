ee5b576a9b7bf0234c51b821a542ce563cdb0b2b
0x0bloodyknight <0x0bloodyknight@gmail.com>
2020-09-12 04:31:50 +0300

    make example settings more readable

--- example_settings.py
+++ example_settings.py
@@ -1,8 +1,8 @@
 #API ID from Telegram Custom Client
-api_id = XXXXXXX
+api_id = 1555551
 
 #API hash from Telegram Custom Client
-api_hash = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+api_hash = 'bf9692222265274g2s4gbfdaes542544f'
 
 #Bot token from @Botfather
-bot_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+bot_token = '4344552343:BBH2Yifhi2e6T8FB95ggPH5c2AfsQYeNl1s'
